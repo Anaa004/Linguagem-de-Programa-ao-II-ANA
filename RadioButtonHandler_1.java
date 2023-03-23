4d37ecb92099da0d366fe8af6576ddbcc3d32f89
Anaa004 <124307084+Anaa004@users.noreply.github.com>
2023-03-23 17:30:52 -0300

    Add files via upload

---
+++ RadioButtonHandler_1.java
@@ -0,0 +1,19 @@
+
+import java.awt.Font;
+import java.awt.event.ItemListener;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class RadioButtonHandler implements ItemListener {
+
+    public RadioButtonHandler(Font plainFont) {
+    }
+    
+}
